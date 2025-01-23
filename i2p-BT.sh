b1189d86e4dffb535f228c3b0e29b552070826b4
wisniaklo <60445509+wisniaklo@users.noreply.github.com>
2025-01-23 21:27:04 +0100

    Update i2p-BT.sh

--- i2p-BT.sh
+++ i2p-BT.sh
@@ -11,13 +11,11 @@ apt-get install -y default-jdk
 adduser i2p
 usermod -aG sudo i2p
 
-su i2p
-mkdir /home/i2p/i2p
-#su - i2p -c "mkdir /home/i2p/i2p"
-cd /home/i2p/i2p/
+su - i2p -c "mkdir /home/i2p/i2p"
+su - i2p -c "cd /home/i2p/i2p/"
 
-wget http://i2pplus.github.io/installers/i2pinstall_2.7.0+.exe
-java -jar i2pinstall_2.7.0+.exe -console
+su - i2p -c "wget http://i2pplus.github.io/installers/i2pinstall_2.7.0+.exe"
+su - i2p -c "java -jar i2pinstall_2.7.0+.exe -console"
 
 apt-get -y autoremove
 apt-get -y autoclean
