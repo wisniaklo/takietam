ef7634a52ef80e5578daaec52623569d285982c8
wisniaklo <60445509+wisniaklo@users.noreply.github.com>
2025-01-23 21:05:00 +0100

    Update i2p-BT.sh

--- i2p-BT.sh
+++ i2p-BT.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
+
+
 apt-get update
 apt-get -y upgrade
 
 adduser i2p
 usermod -aG sudo i2p
 
-su - i2p
+sudo i2p
+
 mkdir /home/i2p/i2p
 cd /home/i2p/i2p/
 
-apt-get install -y curl
-apt-get install -y sudo
-apt-get install -y mc
-apt-get install -y wget
-apt-get install -y default-jdk
+sudo apt-get install -y curl
+sudo apt-get install -y sudo
+sudo apt-get install -y mc
+sudo apt-get install -y wget
+sudo apt-get install -y default-jdk
 
 wget http://i2pplus.github.io/installers/i2pinstall_2.7.0+.exe
 java -jar i2pinstall_2.7.0+.exe -console
