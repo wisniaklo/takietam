b632e642e525b8af1d10599052b2e902cf35e2f7
wisniaklo <60445509+wisniaklo@users.noreply.github.com>
2025-01-23 19:09:23 +0100

    Update i2p-BT.sh

--- i2p-BT.sh
+++ i2p-BT.sh
@@ -1,1 +1,30 @@
+#!/usr/bin/env bash
 
+apt-get update && apt-get -y upgrade
+
+useradd -m -p "" nas
+usermod -s /usr/sbin/nologin nas 
+usermod -s /bin/bash nas
+
+apt-get install -y curl
+apt-get install -y sudo
+apt-get install -y mc
+apt-get install -y wget
+apt-get install -y software-properties-common
+
+add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+apt-get update
+apt-get install -y qbittorrent-nox
+
+runuser -u nas qbittorrent-nox
+
+runuser -u nas cat <<EOF >/home/nas/.config/qBittorrent/qBittorrent.conf
+[Preferences]
+WebUI\Password_PBKDF2="@ByteArray(vwm+l7qfq+4W/6pmC8JRDg==:KFvmdrcjlqXS7wkfqHRTv6y/D36V+OCrr/FSfeo0ISNEPD1uWsV9+pGyqAPkRadI2IEnnFOmYZ7uWOvi8QPRcg==)"
+EOF
+
+systemctl start qbittorrent-nox@nas
+systemctl enable qbittorrent-nox@nas
+
+apt-get -y autoremove
+apt-get -y autoclean
