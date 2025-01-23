57c4ec6495dd6e18a7d827b7e4b022d7bbdf52ff
wisniaklo <60445509+wisniaklo@users.noreply.github.com>
2025-01-23 19:58:25 +0100

    Update lxcmapping.sh

--- lxcmapping.sh
+++ lxcmapping.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-# Funkcja do dodawania wierszy (z kontrolą duplikatów)
-dodaj_wiersze() {
+# Funkcja do dodawania wierszy do pliku konfiguracyjnego LXC (z kontrolą duplikatów)
+dodaj_wiersze_lxc() {
   local sciezka_pliku="$1"
   local dodawane_wiersze=$(cat << EOF
 #####################################################
 ###                 MAP UID i GID                 ###
 #####################################################
+
 # uid map: from uid 0 map 1000 uids (in the ct) to the range starting 100000 (on the host), so 0..999 (ct) → 100000..100999 (host)
 lxc.idmap: u 0 100000 1000
 lxc.idmap: g 0 100000 1000
@@ -16,44 +17,63 @@ lxc.idmap: g 1000 1000 1
 # we map the rest of 65535 from 1001 upto 101001, so 1001..65535 → 101001..165535
 lxc.idmap: u 1001 101001 64535
 lxc.idmap: g 1001 101001 64535
+
 #####################################################
 ###               MOUNT POINTS                    ###
 #####################################################
+
 mp0: /mnt/pve/torrent,mp=/mnt/download
+
 #####################################################
 EOF
 )
 
-  # Sprawdzenie, czy wiersze już istnieją w pliku
   if grep -qF "$dodawane_wiersze" "$sciezka_pliku"; then
-    echo "Wiersze już istnieją w pliku $sciezka_pliku. Nie dodaję duplikatów."
+    echo "Wiersze dla LXC już istnieją w pliku $sciezka_pliku. Nie dodaję duplikatów."
   else
     echo "$dodawane_wiersze" >> "$sciezka_pliku"
-    echo "Wiersze zostały dodane do pliku $sciezka_pliku."
+    echo "Wiersze dla LXC zostały dodane do pliku $sciezka_pliku."
   fi
 }
 
-# Sprawdzenie, czy podano argument
+# Funkcja do dodawania wiersza root:1000:1 do subuid/subgid (z kontrolą duplikatów)
+dodaj_subuid_subgid() {
+  local sciezka_pliku="$1"
+  local wiersz="root:1000:1"
+
+  if grep -qF "$wiersz" "$sciezka_pliku"; then
+    echo "Wiersz '$wiersz' już istnieje w pliku $sciezka_pliku. Nie dodaję duplikatu."
+  else
+    echo "$wiersz" >> "$sciezka_pliku"
+    echo "Wiersz '$wiersz' został dodany do pliku $sciezka_pliku."
+  fi
+}
+
+
+# Obsługa argumentów i prośba o nazwę pliku LXC
 if [ -z "$1" ]; then
-  # Prośba o podanie nazwy pliku
-  read -p "Podaj nazwę pliku konfiguracyjnego kontenera LXC (np 101.conf): " nazwa_pliku
-  if [ -z "$nazwa_pliku" ]; then
-    echo "Nie podano nazwy pliku."
+  read -p "Podaj nazwę pliku konfiguracyjnego LXC (np. 101.conf): " nazwa_pliku_lxc
+  if [ -z "$nazwa_pliku_lxc" ]; then
+    echo "Nie podano nazwy pliku LXC."
     exit 1
   fi
 else
-  nazwa_pliku="$1"
+  nazwa_pliku_lxc="$1"
 fi
 
-sciezka_pliku="/etc/pve/lxc/$nazwa_pliku"
+sciezka_pliku_lxc="/etc/pve/lxc/$nazwa_pliku_lxc"
 
-# Sprawdzenie, czy plik istnieje
-if [ ! -f "$sciezka_pliku" ]; then
-  echo "Plik $sciezka_pliku nie istnieje."
+# Sprawdzenie, czy plik LXC istnieje
+if [ ! -f "$sciezka_pliku_lxc" ]; then
+  echo "Plik $sciezka_pliku_lxc nie istnieje."
   exit 1
 fi
 
-# Wywołanie funkcji dodającej wiersze
-dodaj_wiersze "$sciezka_pliku"
+# Dodawanie wierszy do pliku konfiguracyjnego LXC
+dodaj_wiersze_lxc "$sciezka_pliku_lxc"
+
+# Dodawanie wiersza do /etc/subuid i /etc/subgid
+dodaj_subuid_subgid "/etc/subuid"
+dodaj_subuid_subgid "/etc/subgid"
 
 exit 0
