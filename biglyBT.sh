862631f7328a8ebe767058daee5507c83617aae2
wisniaklo <60445509+wisniaklo@users.noreply.github.com>
2025-01-26 19:41:41 +0100

    Update biglyBT.sh

--- biglyBT.sh
+++ biglyBT.sh
@@ -1,1 +1,20 @@
 
+#!/usr/bin/env bash
+
+apt-get update
+apt-get -y upgrade
+apt-get install -y curl
+apt-get install -y sudo
+apt-get install -y mc
+apt-get install -y wget
+apt-get install -y biglybt
+
+adduser nas
+usermod -aG sudo nas
+
+mkdir -p /home/i2p/i2p
+chown i2p:i2p /home/i2p/i2p
+
+
+apt-get -y autoremove
+apt-get -y autoclean
