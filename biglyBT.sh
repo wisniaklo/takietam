0f3947ef490ff086a301f2bf6828d3c8bbe06cab
wisniaklo <60445509+wisniaklo@users.noreply.github.com>
2025-01-27 15:33:53 +0100

    Update biglyBT.sh

--- biglyBT.sh
+++ biglyBT.sh
@@ -8,9 +8,10 @@ apt-get install -y sudo
 apt-get install -y mc
 apt-get install -y wget
 
-adduser nas
-usermod -aG sudo nas
-#sudo -u nas apt-get install -y biglybt
+#adduser nas
+#usermod -aG sudo nas
+apt-get install -y default-jre
+sudo -u nas apt-get install -y biglybt
 
 apt-get -y autoremove
 apt-get -y autoclean
