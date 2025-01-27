42712a1d298b1ebf40209f2ff332e9eecc1a57a7
wisniaklo <60445509+wisniaklo@users.noreply.github.com>
2025-01-27 18:25:38 +0100

    Update UbuntuHelper.sh

--- UbuntuHelper.sh
+++ UbuntuHelper.sh
@@ -1,109 +1,94 @@
 #!/usr/bin/env bash
 
-
-
-#!/bin/bash
-
 # Wyświetlenie menu z opcjami
 echo "Wybierz jedną z opcji:"
-echo "1. Post install- zainstalowanie potrzebnych pakietów"
-echo "2. Instalacja Desktop GUI"
-echo "3. Odinstaluj Desktop GUI"
-echo "4. Install BiglyBT"
-echo "5. Install Desktop"
+echo "1. Post install - zainstalowanie potrzebnych pakietów"
+echo "2. Instalacja Desktop GUI (LXQt)"
+echo "3. Odinstaluj Desktop GUI (LXQt)"
+echo "4. Instalacja BiglyBT"
+echo "5. Instalacja Desktop (IceWM)"
 echo "exit"
 
 # Odczytanie wyboru użytkownika
 read -p "Twój wybór: " wybor
 
-
 # Sprawdzenie poprawności wyboru
-while [[ "$wybor" != "1" && "$wybor" != "2" && "$wybor" != "3" && "$wybor" != "4" && "$wybor" != "5" && "$wybor" != "exit" ]]; do
-  echo "Niepoprawny wybór. Wybierz liczbę od 1 do 5 lub exit."
-  read -p "Twój wybór: " wybor
+while [[ ! "$wybor" =~ ^[1-5]$ ]] && [[ "$wybor" != "exit" ]]; do
+    echo "Niepoprawny wybór. Wybierz liczbę od 1 do 5 lub exit."
+    read -p "Twój wybór: " wybor
 done
 
 # Wykonanie komend w zależności od wyboru
-case $wybor in
-  1)
-    echo "Wybrano opcję pierwszą"
-    # Tutaj umieść komendy dla opcji pierwszej
-      apt-get update
-      apt-get -y upgrade
-      apt-get install -y curl
-      apt-get install -y sudo
-      apt-get install -y wget
-      #apt-get install -y mc
-      apt-get install -y qemu-guest-agent 
-      apt-get install -y nano
-      apt-get clean -y
-      apt-get autoclean -y
-      apt-get autoremove --purge -y
-      fstrim -av
-    ;;
-  2)
-    echo "Wybrano opcję drugą"
-    # Tutaj umieść komendy dla opcji drugiej
-      apt-get update
-      apt-get -y upgrade
-      #apt-get install -y slim
-      apt-get install lxqt
-      apt-get install sddm
-      #apt-get install -y task-lxqt-desktop
-      reboot
-    ;;
-  3)
-    echo "Wybrano opcję trzecią"
-    # Tutaj umieść komendy dla opcji trzeciej
-      apt-get update
-      apt-get -y upgrade
-      apt-get remove slim lxqt
-      apt-get clean -y
-      apt-get autoclean -y
-      apt-get autoremove --purge -y
-      reboot
-    ;;
-  4)
-    echo "Wybrano opcję trzecią"
-    # Tutaj umieść komendy dla opcji trzeciej
-      #wget https://files.biglybt.com/installer/BiglyBT_Installer.sh
-      #chmod +x BiglyBT_Installer.sh
-      #./BiglyBT_Installer.sh
-      #rm BiglyBT_Installer.sh
-      apt-get install -y default-jre
-      sudo -u nas apt-get install -y biglybt
-      cat << EOF > /etc/systemd/system/biglybt.service
-      [Unit]
-      Description=Opis serwisu
-      After=network.target
-      
-      [Service]
-      Type=simple
-      ExecStart=/home/nas/biglybt/biglybt
-      Restart=always
-      
-      [Install]
-      WantedBy=multi-user.target
-      EOF
-    ;;
-  5)
-    echo "Wybrano opcję trzecią"
-    # Tutaj umieść komendy dla opcji trzeciej
-      apt-get install -y icewm
-      apt-get install -y firefox pcmandfm
-      apt-get install -y nitrogen
-      apt-get install -y ubuntu-wallpapers-jammy
-      apt-get install -y xterm
-      apt-get install -y lxterminal
-      apt-get install -y xinit
-      startx
-      apt-get clean -y
-      apt-get autoclean -y
-      apt-get autoremove --purge -y
-    ;;
-  6)
-    echo "Wybrano opcję czwartą"
-    # Tutaj umieść komendy dla opcji czwartej
-      exit
-    ;;
+case "$wybor" in
+    1)
+        echo "Wybrano opcję pierwszą (Post install)"
+        apt-get update
+        apt-get -y upgrade
+        apt-get install -y curl sudo wget qemu-guest-agent nano
+        apt-get clean
+        apt-get autoclean
+        apt-get autoremove --purge -y
+        fstrim -av
+        ;;
+    2)
+        echo "Wybrano opcję drugą (Instalacja Desktop GUI - LXQt)"
+        apt-get update
+        apt-get -y upgrade
+        apt-get install -y lxqt sddm
+        reboot
+        ;;
+    3)
+        echo "Wybrano opcję trzecią (Odinstaluj Desktop GUI - LXQt)"
+        apt-get update
+        apt-get -y upgrade
+        apt-get remove --purge -y lxqt sddm
+        apt-get clean
+        apt-get autoclean
+        apt-get autoremove --purge -y
+        reboot
+        ;;
+    4)
+        echo "Wybrano opcję czwartą (Instalacja BiglyBT)"
+        apt-get update
+        apt-get -y install default-jre
+        # Upewnij się, że użytkownik "nas" istnieje i ma odpowiednie uprawnienia
+        if ! id -u nas >/dev/null 2>&1; then
+            echo "Użytkownik 'nas' nie istnieje. Utwórz go najpierw."
+            exit 1
+        fi
+        sudo -u nas apt-get install -y biglybt
+
+        cat << EOF > /etc/systemd/system/biglybt.service
+[Unit]
+Description=BiglyBT service
+After=network.target
+
+[Service]
+Type=simple
+User=nas # Dodano, aby usługa działała jako użytkownik nas
+ExecStart=/home/nas/biglybt/biglybt
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+        sudo systemctl daemon-reload
+        sudo systemctl enable biglybt
+        sudo systemctl start biglybt
+        ;;
+    5)
+        echo "Wybrano opcję piątą (Instalacja Desktop - IceWM)"
+        apt-get update
+        apt-get -y install icewm firefox pcmandfm nitrogen ubuntu-wallpapers-jammy xterm lxterminal xinit
+        #Dodanie startx do bashrc
+        echo "if [ -z \"\$DISPLAY\" ] && [ \$(tty) = /dev/tty1 ]; then startx; fi" >> ~/.bashrc
+        ;;
+    exit)
+        echo "Koniec skryptu."
+        exit 0
+        ;;
+    *) # Domyślny przypadek (nigdy nie powinien wystąpić dzięki pętli while)
+        echo "Nieznana opcja."
+        exit 1
+        ;;
 esac
