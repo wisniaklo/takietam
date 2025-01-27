2d3503598423f8b160da78151b52ae8bb14762e9
wisniaklo <60445509+wisniaklo@users.noreply.github.com>
2025-01-27 11:05:16 +0100

    Update postInstall.sh

--- postInstall.sh
+++ postInstall.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
 
+apt-get update
+apt-get -y upgrade
+apt-get install -y curl
+apt-get install -y sudo
+apt-get install -y wget
+#apt-get install -y mc
+apt-get install -y qemu-guest-agent 
+apt-get install -y nano
+apt-get clean -y
+apt-get autoclean -y
+apt-get autoremove --purge -y
