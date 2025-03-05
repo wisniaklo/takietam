9eef702855e68e5c7d12e2a2ba95355805139f5c
wisniaklo <60445509+wisniaklo@users.noreply.github.com>
2025-03-05 15:28:18 +0100

    Update installhelpers.sh

--- installhelpers.sh
+++ installhelpers.sh
@@ -65,13 +65,16 @@ EOF
         ;;
     5)
         echo "Wybrano opcję piątą (Instalacja BiglyBT)"
-        apt-get update
-        apt-get -y upgrade
-        apt-get -y install default-jre
+        sudo apt-get update
+        sudo apt-get -y upgrade
+        sudo apt-get -y install default-jre
         wget https://files.biglybt.com/installer/BiglyBT_Installer.sh
         chmod +x BiglyBT_Installer.sh
-        sudo -u nas ./BiglyBT_Installer.sh
-        cat << EOF > /etc/systemd/system/biglybt.service
+
+        #sudo -u nas ./BiglyBT_Installer.sh
+        sudo ./BiglyBT_Installer.sh
+        #sudo -u nas cat << EOF > /etc/systemd/system/biglybt.service
+        sudo cat << EOF > /etc/systemd/system/biglybt.service
 [Unit]
 Description=BiglyBTd (BiglyBT as a system service)
 After=network-online.target
@@ -95,13 +98,13 @@ EOF
         ;;
     6)
         echo "Wybrano opcję szustą (Instalacja Cloudflare WARP)"
-        apt-get update
-        apt-get -y upgrade
+        sudo apt-get update
+        sudo apt-get -y upgrade
         curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
         echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(grep VERSION_CODENAME /etc/os-release | cut -d= -f2) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
-        apt-get update
-        apt-get install cloudflare-warp
-        sysctl -w net.ipv4.ip_forward=1
+        sudo apt-get update
+        sudo apt-get install cloudflare-warp
+        sudo sysctl -w net.ipv4.ip_forward=1
         sudo -u nas warp-cli connector new eyJhIjoiNjJiZmU4MTgxN2IwNDAwN2NlODIyMzMwYTY1NjNiNGQiLCJ0IjoiOGJkYjE0MjQtNjAwZS00ZDY1LTgxZGYtOWExNzJhNjc0OWFmIiwicyI6Ik9EazVNakF6TUdZdE0yTXhNaTAwTUdJeUxXSXpZbVF0WldJeFltWmhZelZrTXpnMSJ9
         sudo -u nas warp-cli connect
         ;;
